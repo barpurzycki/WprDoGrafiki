8b45a2d465e74e11d8f0bf0382c5d7c0ebb11a9a
barpurzycki <92019696+barpurzycki@users.noreply.github.com>
2023-10-11 12:55:25 +0200

    Update lab2cw.py

--- lab2/lab2cw.py
+++ lab2/lab2cw.py
@@ -27,15 +27,31 @@ def rysuj_ramke_w_obrazie(obraz, grub):
 
 inicjaly_ramka5 = rysuj_ramke_w_obrazie(inicjaly, 5)
 
-inicjaly_ramka5.show()
+#inicjaly_ramka5.show()
 
-inicjaly_ramka5.save("ramka5.bmp")
+#inicjaly_ramka5.save("ramka5.bmp")
 
 #Ramka grubosc 10
 
 inicjaly_ramka10 = rysuj_ramke_w_obrazie(inicjaly, 10)
 
-inicjaly_ramka10.show()
+#inicjaly_ramka10.show()
 
-inicjaly_ramka10.save("ramka10.bmp")
+#inicjaly_ramka10.save("ramka10.bmp")
+
+#Zadanie 3.
+
+def rysuj_ramke(w, h, grub): # grub grubość ramki w pikselach
+    t = (h, w)  # rozmiar tablicy
+    tab = np.zeros(t, dtype=np.uint8)  # deklaracja tablicy wypełnionej zerami - czarna
+    tab[grub:h - grub, grub:w - grub] = 1  # skrócona wersja ustawienia wartości dla prostokatnego fragmentu tablicy [zakresy wysokości, zakresy szerokości] tablicy
+    tab1 = tab.astype(bool)
+    #return tab * 255  # alternatywny sposób uzyskania tablicy obrazu czarnobiałego ale w trybie odcieni szarości
+    return tab1
+
+tab = rysuj_ramke(120, 60, 8)
+print("typ danych tablicy", tab.dtype)
+print("rozmiar wyrazu tablicy:",   tab.itemsize)
+im_ramka = Image.fromarray(tab)
+im_ramka.show()
 
