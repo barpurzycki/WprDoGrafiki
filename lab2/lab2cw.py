cc8fef540f316302f8213fbc67fc1884c8052bc4
barpurzycki <92019696+barpurzycki@users.noreply.github.com>
2023-10-15 16:17:01 +0200

    Update lab2cw.py

--- lab2/lab2cw.py
+++ lab2/lab2cw.py
@@ -27,31 +27,100 @@ def rysuj_ramke_w_obrazie(obraz, grub):
 
 inicjaly_ramka5 = rysuj_ramke_w_obrazie(inicjaly, 5)
 
-#inicjaly_ramka5.show()
+inicjaly_ramka5.show()
 
-#inicjaly_ramka5.save("ramka5.bmp")
+inicjaly_ramka5.save("ramka5.bmp")
 
 #Ramka grubosc 10
 
 inicjaly_ramka10 = rysuj_ramke_w_obrazie(inicjaly, 10)
 
-#inicjaly_ramka10.show()
+inicjaly_ramka10.show()
 
-#inicjaly_ramka10.save("ramka10.bmp")
+inicjaly_ramka10.save("ramka10.bmp")
 
 #Zadanie 3.
 
-def rysuj_ramke(w, h, grub): # grub grubość ramki w pikselach
-    t = (h, w)  # rozmiar tablicy
-    tab = np.zeros(t, dtype=np.uint8)  # deklaracja tablicy wypełnionej zerami - czarna
-    tab[grub:h - grub, grub:w - grub] = 1  # skrócona wersja ustawienia wartości dla prostokatnego fragmentu tablicy [zakresy wysokości, zakresy szerokości] tablicy
+#Podpunkt 1.1.
+
+#Podpunkt 1.2.
+
+def rysuj_pasy_pionowe(w, h, grub):
+    t = (h, w)
+    tab = np.zeros(t, dtype=np.uint8)
+    ile =  int(w/grub)
+    for k in range(ile):  # uwaga k = 0,1,2,3,4,5,8   bez dziewiatki
+        for g in range(grub):
+            i = k * grub + g  # i - indeks wiersza, j - indeks kolumny
+            for j in range(h):
+                tab[j, i] = k % 2
+    tab = tab * 255
+    return Image.fromarray(tab)  # tworzy obraz
+
+obraz12 = rysuj_pasy_pionowe(480, 320, 10)
+
+obraz12.show()
+
+obraz12.save("obraz2.bmp")
+
+#Podpunkt 1.3.
+
+def rysuj_prostokaty(w, h, m, n):
+    t = (h, w)
+    tab = np.ones(t, dtype=np.uint8)
+    tab[0:n, 0:m] = 0
+    tab[n:h, m:w] = 0
+    tab = tab.astype(bool)
+    return tab
+
+obraz13 = rysuj_prostokaty(480,320, 100, 50)
+obraz13_obr = Image.fromarray(obraz13)
+obraz13_obr.show()
+
+obraz13_obr.save("obraz3.bmp")
+
+#Podpunkt 1.4.
+
+#Obraz,w którym ramka nachodzi z góry i z lewej strony
+def rysuj_ramke_lewo_gora(w, h, grub):
+    t = (h, w)
+    tab = np.ones(t, dtype=np.uint8)
+    for i in range(h):
+        for j in range(grub):
+            tab[i][j]=0
+    for a in range(w):
+        for b in range(grub):
+            tab[b][a]=0
     tab1 = tab.astype(bool)
-    #return tab * 255  # alternatywny sposób uzyskania tablicy obrazu czarnobiałego ale w trybie odcieni szarości
-    return tab1
+    return Image.fromarray(tab1)
 
-tab = rysuj_ramke(120, 60, 8)
-print("typ danych tablicy", tab.dtype)
-print("rozmiar wyrazu tablicy:",   tab.itemsize)
-im_ramka = Image.fromarray(tab)
-im_ramka.show()
+obraz14 = rysuj_ramke_lewo_gora(480, 320, 10)
+obraz14.show()
 
+obraz14.save("obraz4.bmp")
+
+#Zadanie 5.
+
+def wstaw_obraz_w_obraz(obraz_bazowy, obraz_wstawiany, m, n):
+    tab_obraz = np.asarray(obraz_wstawiany)*1
+    h0, w0 = tab_obraz.shape
+    tab_obraz_bazowy = np.asarray(obraz_bazowy)*1
+    h1, w1 = tab_obraz_bazowy.shape
+    n_k = min(h1, n + h0)
+    m_k = min(w1, m + w0)
+    n_p = max(0, n)
+    m_p = max(0, m)
+    print(n_k, m_k)
+    print(n_p, m_p)
+    for i in range(n_p, n_k):
+        for j in range(m_p, m_k):
+            tab_obraz_bazowy[i][j] = tab_obraz[i - n][j - m]
+    tab = tab_obraz_bazowy.astype(bool)
+    return Image.fromarray(tab)
+
+obraz12zad5 = Image.open("obraz2.bmp")
+obrazzad5 = wstaw_obraz_w_obraz(obraz12zad5, inicjaly, 300, 90)
+obrazzad5.save("wstaw1.bmp")
+
+obrazzad52 = wstaw_obraz_w_obraz(obraz12zad5, inicjaly, 10, 290)
+obrazzad52.save("wstaw2.bmp")
