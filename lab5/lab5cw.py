6005550e32dfd87d3dc706f0a324a6ce0dd92bf9
barpurzycki <92019696+barpurzycki@users.noreply.github.com>
2023-11-18 17:54:20 +0100

    Update lab5cw.py

--- lab5/lab5cw.py
+++ lab5/lab5cw.py
@@ -5,7 +5,7 @@ from PIL import ImageStat as stat
 import matplotlib.pyplot as plt
 from random import randint
 
-#Zad 1.
+# Zad 1.
 
 im3_1 = Image.open('im3.jpg')
 im3_2 = Image.open('im3.png')
@@ -14,6 +14,7 @@ diff_im3 = ImageChops.difference(im3_1, im3_2)
 
 diff_im3.save("diff.png")
 
+#Podpunkt a.
 def statystyki(im):
     s = stat.Stat(im)
     print("extrema ", s.extrema)  # max i min
@@ -22,8 +23,10 @@ def statystyki(im):
     print("median ", s.median)  # mediana
     print("stddev ", s.stddev)  # odchylenie standardowe
 
+
 statystyki(diff_im3)
 
+#Podpunkt b.
 def rysuj_histogram_RGB(obraz):
     hist = obraz.histogram()
     plt.title("histogram  ")
@@ -33,44 +36,54 @@ def rysuj_histogram_RGB(obraz):
     plt.savefig("histogram1.png")
     plt.show()
 
-#rysuj_histogram_RGB(diff_im3)
 
-def zlicz_roznice_srednia_RGB(obraz, wsp): # wsp - współczynnik określający dokładność oceny
+rysuj_histogram_RGB(diff_im3)
+
+#Podpunkt c.
+def zlicz_roznice_srednia_RGB(obraz, wsp):  # wsp - współczynnik określający dokładność oceny
     t_obraz = np.asarray(obraz)
     h, w, d = t_obraz.shape
     zlicz = 0
     for i in range(h):
         for j in range(w):
-                if np.mean(t_obraz[i, j, :]) > wsp:
-                    zlicz = zlicz + 1
-    procent = zlicz/(h*w)
+            if np.mean(t_obraz[i, j, :]) > wsp:
+                zlicz = zlicz + 1
+    procent = zlicz / (h * w)
     return zlicz, procent
+
 
 print(zlicz_roznice_srednia_RGB(diff_im3, 80))
 print(zlicz_roznice_srednia_RGB(diff_im3, 50))
 print(zlicz_roznice_srednia_RGB(diff_im3, 20))
 
-def zlicz_roznice_suma_RGB(obraz, wsp): # wsp - współczynnik określający dokładność oceny
+#Podpunkt d.
+def zlicz_roznice_suma_RGB(obraz, wsp):  # wsp - współczynnik określający dokładność oceny
     t_obraz = np.asarray(obraz)
     h, w, d = t_obraz.shape
     zlicz = 0
     for i in range(h):
         for j in range(w):
-                if sum(t_obraz[i, j, :]) > wsp: # poniżej równoważne sformułowania tego warunku
+            if sum(t_obraz[i, j, :]) > wsp:  # poniżej równoważne sformułowania tego warunku
                 # if (t_obraz[i, j, 0] + t_obraz[i, j, 1] + t_obraz[i, j, 2]) > wsp:
                 # if t_obraz[i, j, 0] > wsp or  t_obraz[i, j, 1] > wsp or t_obraz[i, j, 2] > wsp:
-                    zlicz = zlicz + 1
-    procent = zlicz/(h*w)
+                zlicz = zlicz + 1
+    procent = zlicz / (h * w)
     return zlicz, procent
+
 
 print(zlicz_roznice_suma_RGB(diff_im3, 80))
 print(zlicz_roznice_suma_RGB(diff_im3, 50))
 print(zlicz_roznice_suma_RGB(diff_im3, 20))
 
-#Zad 2.
+# Zad 2.
+
+#Podpunkt a.
 
 im1 = Image.open("obraz.jpg")
 im1.save("obraz1.jpg")
+
+#Podpunkt b.
+
 im1_1 = Image.open("obraz1.jpg")
 im1_1.save("obraz2.jpg")
 im1_2 = Image.open("obraz2.jpg")
@@ -80,24 +93,38 @@ im1_3.save("obraz4.jpg")
 im1_4 = Image.open("obraz4.jpg")
 im1_4.save("obraz5.jpg")
 
+#Podpunkt c.
+
 diff_im1 = ImageChops.difference(im1, im1_4)
 diff_im1.save("diffzad2.png")
+
+#Podpunkt d.
+
 diff_im1_1 = ImageChops.difference(im1_3, im1_4)
 diff_im1_1.save("diffzad2przyk2.png")
 
-#Zad 3.
 
-def ukryj_kod(obraz, im_kod):
-    t_obraz = np.asarray(obraz)
-    t_kodowany = t_obraz.copy()
-    h, w, d = t_obraz.shape
-    t_kod = np.asarray(im_kod)
+# Zad 3.
+
+#Podpunkt a.
+def odkoduj(obraz1, obraz2):
+    t_obraz1 = np.asarray(obraz1)
+    t_obraz2 = np.asarray(obraz2)
+    h, w, d = t_obraz1.shape
+    obraz = np.zeros((h, w), dtype=np.uint8)
     for i in range(h):
         for j in range(w):
-            if t_kod[i, j] > 0:
-                k = randint(0,2)
-                t_kodowany[i, j, k] = t_obraz[i, j, k] + 1
-    return Image.fromarray(t_kodowany)
+            if np.array_equal(t_obraz1[i, j], t_obraz2[i, j]):
+                obraz[i, j] = 0
+            else:
+                obraz[i, j] = 255
+    return Image.fromarray(obraz, mode='L')
 
-def odkoduj(obraz1, obraz2):
-    
+#Podpunkt b.
+
+obr1zad3 = Image.open("jesien.jpg")
+obr2zad3 = Image.open("zakodowany2.bmp")
+
+zadanie3 = odkoduj(obr1zad3, obr2zad3)
+zadanie3.save("kod2.bmp")
+zadanie3.show()
